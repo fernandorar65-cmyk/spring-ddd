8d5d810f7aa7cbcd1ada2131da200896fe55baf2
FernandoRar <fernandorar65@gmail.com>
2026-08-04 15:24:35 -0500

    add: tables for gamplay

--- src/main/java/kahoot/clabs/kahoot_clabs/quiz/infrastructure/persistence/QuizEntity.java
+++ src/main/java/kahoot/clabs/kahoot_clabs/quiz/infrastructure/persistence/QuizEntity.java
@@ -43,6 +43,7 @@ public class QuizEntity {
     @Column(name = "thumbnail_url", length = 500)
     private String thumbnailUrl;
 
+    // estado de draft - published - archived - off entre otros considerar volverlo una tabla
     @Column(nullable = false, length = 20)
     private String status;
 
